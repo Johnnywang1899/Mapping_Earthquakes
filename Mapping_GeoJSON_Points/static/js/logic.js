347f5b8f0fef7802f38eb9dfd10df78ccb2a2b77
Yining (Johnny) Wang <yining.wang@mail.utoronto.ca>
2021-03-23 13:42:31 -0400

    base maps layout added

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,8 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+//let map = L.map('mapid').setView([30, 30], 2);
+
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -38,7 +39,7 @@ let myLine = [
 L.polyline(myLine, {
     color : "red"
 }).addTo(map);
-*/
+
 // Add GeoJSON data.
 let sanFranAirport =
 {"type":"FeatureCollection","features":[{
@@ -58,7 +59,7 @@ let sanFranAirport =
             "type":"Point",
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
-
+*/
 /*
 // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
@@ -70,8 +71,24 @@ L.geoJSON(sanFranAirport, {
     }
 }).addTo(map);
 */
+// Accessing the air port GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Johnnywang1899/Mapping_Earthquakes/main/majorAirports.json";
 
-// Grabbing our GeoJSON data.
+//Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data){
+    console.log(data);
+    //Creating a GeoJSON layer with the retrieved data.
+    //data.forEach(function (item){
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng){
+            return L.marker(latlng).bindPopup(feature.properties.faa);
+        }
+    }).addTo(map);
+    //})
+    
+});
+
+/* Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
     onEachFeature: function(feature, layer){
         console.log(feature);
@@ -79,6 +96,14 @@ L.geoJSON(sanFranAirport, {
         layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2>" + "<hr>" + "<h3>" + "Airport name: " + feature.properties.name + "</h3>");
     }
 }).addTo(map);
+*/
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -87,6 +112,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// Create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
 
+let map = L.map("mapid", {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
